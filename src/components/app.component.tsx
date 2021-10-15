3b6d32aea8cce07bab2bad163bf62dae768ea9f6
Max Maximov <maxim.maximov@netcracker.com>
2021-10-15 10:09:01 +0300

    initial commit

---
+++ src/components/app.component.tsx
@@ -0,0 +1,23 @@
+import React, { memo } from 'react';
+import logo from '../logo.svg';
+import styles from './app.module.scss';
+
+type AppProps = {};
+
+const App = memo<AppProps>(function App() {
+    return (
+        <div className={styles.App}>
+            <header className={styles.AppHeader}>
+                <img src={logo} className={styles.AppLogo} alt='logo' />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a className={styles.AppLink} href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+});
+
+export default App;
